9bdc91bc6a2cdb310ffbf349297e664edd332251
Kamil <kamilsimsek420@gmail.com>
2022-12-12 19:28:45 +0300

    First infrastructers

---
+++ WebTechnologiesProject/Infrastructure/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebTechnologiesProject.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
