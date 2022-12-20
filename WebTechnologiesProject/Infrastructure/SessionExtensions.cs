4dd20ff059ca470fe4f4258fc690265f14e74202
Kamil <kamilsimsek420@gmail.com>
2022-12-20 22:35:45 +0300

    Update1

---
+++ WebTechnologiesProject/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace WebTechnologiesProject.Infrastructure
+{
+    public class SessionExtensions
+    {
+        public static void SetJson(ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConverter.SerializeObject(value));
+        }
+    }
+}
